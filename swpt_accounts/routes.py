7aeafed4375d7d507e8620e83152e81592442e4f
Evgeni Pandurski <epandurski@gmail.com>
2019-06-19 20:14:14 +0300

    Fix bug

--- swpt_accounts/routes.py
+++ swpt_accounts/routes.py
@@ -39,8 +39,8 @@ web_api.add_url_rule('/accounts/<int:debtor_id>/<int:creditor_id>/', view_func=A
 
 @web_api.route('/staled-transfers/<int:debtor_id>/', methods=['GET'])
 def get_staled_transfers(debtor_id):
-    days = int(request.args.get('days', '30'))
+    days = int(request.args.get('days', '7'))
     current_ts = datetime.now(tz=timezone.utc)
-    staled_transfers = procedures.get_staled_transfers(debtor_id, current_ts + timedelta(days=days))
+    staled_transfers = procedures.get_staled_transfers(debtor_id, current_ts - timedelta(days=days))
     staled_transfers_json = json.dumps([prepared_transfer_schema.dump(pt) for pt in staled_transfers])
     return staled_transfers_json, 200, {'Content-Type': 'application/json'}
