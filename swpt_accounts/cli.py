e16c94007e3b99e5b502276f95d3963d052fc1d4
Evgeni Pandurski <epandurski@gmail.com>
2019-06-29 18:28:21 +0300

    Add a subscribe CLI command

--- swpt_accounts/cli.py
+++ swpt_accounts/cli.py
@@ -42,3 +42,40 @@ def process_pending_changes(threads):
         pool.apply_async(procedures.process_pending_changes, account_pk, error_callback=log_error)
     pool.close()
     pool.join()
+
+
+@swpt_accounts.command()
+@with_appcontext
+@click.argument('queue_name')
+def subscribe(queue_name):
+    """Subscribe a queue for the observed events and messages.
+
+    QUEUE_NAME specifies the name of the queue.
+
+    """
+
+    from .extensions import broker, DRAMATIQ_EXCHANGE_NAME
+    from . import actors  # noqa
+
+    channel = broker.channel
+    channel.exchange_declare(DRAMATIQ_EXCHANGE_NAME)
+    click.echo(f'Declared "{DRAMATIQ_EXCHANGE_NAME}" direct exchange.')
+
+    if environ.get('APP_USE_LOAD_BALANCING_EXCHANGE', ''):
+        bind = channel.exchange_bind
+        unbind = channel.exchange_unbind
+    else:
+        bind = channel.queue_bind
+        unbind = channel.queue_unbind
+    bind(queue_name, DRAMATIQ_EXCHANGE_NAME, queue_name)
+    click.echo(f'Subscribed "{queue_name}" to "{DRAMATIQ_EXCHANGE_NAME}.{queue_name}".')
+
+    for actor in [broker.get_actor(actor_name) for actor_name in broker.get_declared_actors()]:
+        if 'event_subscription' in actor.options:
+            routing_key = f'events.{actor.actor_name}'
+            if actor.options['event_subscription']:
+                bind(queue_name, DRAMATIQ_EXCHANGE_NAME, routing_key)
+                click.echo(f'Subscribed "{queue_name}" to "{DRAMATIQ_EXCHANGE_NAME}.{routing_key}".')
+            else:
+                unbind(queue_name, DRAMATIQ_EXCHANGE_NAME, routing_key)
+                click.echo(f'Unsubscribed "{queue_name}" from "{DRAMATIQ_EXCHANGE_NAME}.{routing_key}".')
