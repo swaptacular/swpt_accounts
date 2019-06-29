4bfd695d803db625a0b5bcd7028c2ef716838fde
Evgeni Pandurski <epandurski@gmail.com>
2019-06-29 18:19:54 +0300

    Add EventSubscriptionMiddleware to the broker

--- swpt_accounts/extensions.py
+++ swpt_accounts/extensions.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_signalbus import SignalBusMixin, AtomicProceduresMixin
 from flask_melodramatiq import RabbitmqBroker
+from dramatiq import Middleware
 
 DRAMATIQ_EXCHANGE_NAME = 'dramatiq'
 APP_QUEUE_NAME = os.environ.get('APP_QUEUE_NAME', 'swpt_accounts')
@@ -20,6 +21,13 @@ class CustomAlchemy(AtomicProceduresMixin, SignalBusMixin, SQLAlchemy):
     pass
 
 
+class EventSubscriptionMiddleware(Middleware):
+    @property
+    def actor_options(self):
+        return {'event_subscription'}
+
+
 db = CustomAlchemy()
 migrate = Migrate()
 broker = RabbitmqBroker(confirm_delivery=True)
+broker.add_middleware(EventSubscriptionMiddleware())
