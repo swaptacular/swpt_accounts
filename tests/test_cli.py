48efa44aff5133ab65c7f4e5a4d7c9dd26181b10
Evgeni Pandurski <epandurski@gmail.com>
2019-06-29 18:03:05 +0300

    Fix flask CLI command name

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,7 +9,7 @@ def test_process_pending_changes(app, db_session):
     assert p.get_available_balance(D_ID, C_ID, ignore_interest=True) == 0
     assert p.get_available_balance(D_ID, p.ROOT_CREDITOR_ID, ignore_interest=True) == 0
     runner = app.test_cli_runner()
-    result = runner.invoke(args=['swpt-accounts', 'process-pending-changes'])
+    result = runner.invoke(args=['swpt_accounts', 'process-pending-changes'])
     assert not result.output
     assert p.get_available_balance(D_ID, C_ID, ignore_interest=True) == 1000
     assert p.get_available_balance(D_ID, p.ROOT_CREDITOR_ID, ignore_interest=True) == -1000
