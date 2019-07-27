52451a0989cc191003f3f6e9a94504b49d3b3fd6
Evgeni Pandurski <epandurski@gmail.com>
2019-07-27 23:49:35 +0300

    Use one command (process_transfers) instead of two

--- swpt_accounts/cli.py
+++ swpt_accounts/cli.py
@@ -12,43 +12,17 @@ def swpt_accounts():
     """Perform operations on Swaptacular accounts."""
 
 
-@swpt_accounts.command()
+@swpt_accounts.command('process_transfers')
 @with_appcontext
 @click.option('-t', '--threads', type=int, help='The number of worker threads.')
-def process_transfer_requests(threads):
-    """Process all transfer requests."""
-
-    threads = threads or int(environ.get('APP_TRANSFER_REQUESTS_THREADS', '1'))
-    app = current_app._get_current_object()
-
-    def push_app_context():
-        ctx = app.app_context()
-        ctx.push()
-
-    def log_error(e):  # pragma: no cover
-        try:
-            raise e
-        except Exception:
-            logger = logging.getLogger(__name__)
-            logger.exception('Caught error while processing transfer requests.')
+def process_transfers(threads):
+    """Process all pending account changes and all transfer requests."""
 
     # TODO: Python with SQLAlchemy can process about 1000 accounts per
     # second. (It is CPU bound!) This might be insufficient if we have
     # a highly perfomant database server. In this case we should
     # either distribute the processing to several machines, or improve
     # on python's code performance.
-    pool = ThreadPool(threads, initializer=push_app_context)
-    for account_pk in procedures.get_accounts_with_transfer_requests():
-        pool.apply_async(procedures.process_transfer_requests, account_pk, error_callback=log_error)
-    pool.close()
-    pool.join()
-
-
-@swpt_accounts.command()
-@with_appcontext
-@click.option('-t', '--threads', type=int, help='The number of worker threads.')
-def process_pending_changes(threads):
-    """Process all pending account changes."""
 
     threads = threads or int(environ.get('APP_PENDING_CHANGES_THREADS', '1'))
     app = current_app._get_current_object()
@@ -62,18 +36,19 @@ def process_pending_changes(threads):
             raise e
         except Exception:
             logger = logging.getLogger(__name__)
-            logger.exception('Caught error while processing account pending changes.')
+            logger.exception('Caught error while processing transfers.')
 
-    # TODO: Python with SQLAlchemy can process about 1000 accounts per
-    # second. (It is CPU bound!) This might be insufficient if we have
-    # a highly perfomant database server. In this case we should
-    # either distribute the processing to several machines, or improve
-    # on python's code performance.
-    pool = ThreadPool(threads, initializer=push_app_context)
+    pool1 = ThreadPool(threads, initializer=push_app_context)
     for account_pk in procedures.get_accounts_with_pending_changes():
-        pool.apply_async(procedures.process_pending_changes, account_pk, error_callback=log_error)
-    pool.close()
-    pool.join()
+        pool1.apply_async(procedures.process_pending_changes, account_pk, error_callback=log_error)
+    pool1.close()
+    pool1.join()
+
+    pool2 = ThreadPool(threads, initializer=push_app_context)
+    for account_pk in procedures.get_accounts_with_transfer_requests():
+        pool2.apply_async(procedures.process_transfer_requests, account_pk, error_callback=log_error)
+    pool2.close()
+    pool2.join()
 
 
 @swpt_accounts.command()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,7 +11,7 @@ def test_process_pending_changes(app, db_session):
     assert p.get_available_balance(D_ID, C_ID, ignore_interest=True) == 0
     assert p.get_available_balance(D_ID, p.ROOT_CREDITOR_ID, ignore_interest=True) == 0
     runner = app.test_cli_runner()
-    result = runner.invoke(args=['swpt_accounts', 'process-pending-changes'])
+    result = runner.invoke(args=['swpt_accounts', 'process_transfers'])
     assert not result.output
     assert p.get_available_balance(D_ID, C_ID, ignore_interest=True) == 1000
     assert p.get_available_balance(D_ID, p.ROOT_CREDITOR_ID, ignore_interest=True) == -1000
@@ -30,6 +30,6 @@ def test_process_transfer_requests(app, db_session):
         ignore_interest=False,
     )
     runner = app.test_cli_runner()
-    result = runner.invoke(args=['swpt_accounts', 'process-transfer-requests'])
+    result = runner.invoke(args=['swpt_accounts', 'process_transfers'])
     assert not result.output
     assert RejectedTransferSignal.query.one()
