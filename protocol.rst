513d81ddde3dec6a7931161130f610c531d5e5f4
Evgeni Pandurski <epandurski@gmail.com>
2020-05-19 13:01:25 +0300

    Do not allow creating new account that are schd-for-deletion

--- protocol.rst
+++ protocol.rst
@@ -72,7 +72,7 @@ they MUST first verify whether the specified account already exists:
    an attempt MUST be made to create a new account with the requested
    configuration settings. If the new account has been successfully
    created, an `AccountChange`_ message MUST be sent; otherwise a
-   `RejectedConfig`_ message MUST be sent.
+   `RejectedConfig`_ message MUST be sent. [#schd-deletion]_
 
 .. [#forbid-transfers] Server implementations SHOULD forbid incoming
   transfer for "scheduled for deletion" accounts.
@@ -97,6 +97,9 @@ they MUST first verify whether the specified account already exists:
   seqnum1) % 0x100000000 < 0x80000000``. Timestamps must also be
   compared with care, because precision might have been lost when they
   were saved to the database.
+
+.. [#schd-deletion] Creating a new account with "scheduled for
+  deletion" flag SHOULD NOT be allowed.
 
 
 PrepareTransfer
--- tests/test_procedures.py
+++ tests/test_procedures.py
@@ -78,7 +78,7 @@ def test_invalid_config(db_session, current_ts):
     p.configure_account(D_ID, C_ID, current_ts - timedelta(days=1000), 123)
     assert p.get_account(D_ID, C_ID) is None
     assert len(AccountChangeSignal.query.all()) == 0
-    rcs = RejectedConfigSignal.query.filter_by(rejection_code='OUTDATED_CONFIGURATION').one()
+    rcs = RejectedConfigSignal.query.filter_by(rejection_code='DELETED_ACCOUNT').one()
     assert rcs.debtor_id == D_ID
     assert rcs.creditor_id == C_ID
     assert rcs.config_signal_ts == current_ts - timedelta(days=1000)
@@ -130,7 +130,8 @@ def amount(request):
 
 def test_make_debtor_payment(db_session, current_ts, amount):
     TRANSFER_MESSAGE = '{"transer_data": 123}'
-    p.configure_account(D_ID, C_ID, current_ts, 0,
+    p.configure_account(D_ID, C_ID, current_ts, 0),
+    p.configure_account(D_ID, C_ID, current_ts, 1,
                         status_flags=Account.STATUS_SCHEDULED_FOR_DELETION_FLAG, negligible_amount=abs(amount))
     p.make_debtor_payment('test', D_ID, C_ID, amount, TRANSFER_MESSAGE)
 
