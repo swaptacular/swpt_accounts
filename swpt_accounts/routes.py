e8b4ad96c4c011ee648b35aace2407d96b594f00
Evgeni Pandurski <epandurski@gmail.com>
2020-12-20 20:43:16 +0200

    Pass debtorId first in the fetch_api

--- swpt_accounts/routes.py
+++ swpt_accounts/routes.py
@@ -9,15 +9,15 @@ HTTP_HEADERS = {
 fetch_api = Blueprint('fetch', __name__, url_prefix='/accounts')
 
 
-@fetch_api.route('/<i64:creditorId>/<i64:debtorId>/reachable')
-def reachable(creditorId, debtorId):
+@fetch_api.route('/<i64:debtorId>/<i64:creditorId>/reachable')
+def reachable(debtorId, creditorId):
     is_rachable_account = procedures.is_reachable_account(debtorId, creditorId)
     status_code = 204 if is_rachable_account else 404
     return '', status_code, HTTP_HEADERS
 
 
-@fetch_api.route('/<i64:creditorId>/<i64:debtorId>/config')
-def config(creditorId, debtorId):
+@fetch_api.route('/<i64:debtorId>/<i64:creditorId>/config')
+def config(debtorId, creditorId):
     config_data = procedures.get_account_config_data(debtorId, creditorId)
     status_code = 404 if config_data is None else 200
     return config_data or '', status_code, HTTP_HEADERS
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -23,35 +23,35 @@ def account(app, db_session, current_ts):
 
 
 def test_get_reachable(client, account, current_ts):
-    r = client.post('/accounts/1/18446744073709551615/reachable', json={})
+    r = client.post('/accounts/18446744073709551615/1/reachable', json={})
     assert r.status_code == 405
-    r = client.get('/accounts/1/18446744073709551615/reachable')
+    r = client.get('/accounts/18446744073709551615/1/reachable')
     assert r.status_code == 204
     assert r.get_data() == b''
-    r = client.get('/accounts/1/18446744073709551614/reachable')
+    r = client.get('/accounts/18446744073709551614/1/reachable')
     assert r.status_code == 404
     assert r.get_data() == b''
-    r = client.get('/accounts/0/18446744073709551614/reachable')
+    r = client.get('/accounts/18446744073709551614/0/reachable')
     assert r.status_code == 204
     assert r.get_data() == b''
 
     p.configure_account(D_ID, C_ID, current_ts, 1, config_flags=m.Account.CONFIG_SCHEDULED_FOR_DELETION_FLAG)
-    r = client.get('/accounts/1/18446744073709551615/reachable')
+    r = client.get('/accounts/18446744073709551615/1/reachable')
     assert r.status_code == 404
     assert r.get_data() == b''
 
 
 def test_get_config(client, account, current_ts):
-    r = client.post('/accounts/1/18446744073709551615/config', json={})
+    r = client.post('/accounts/18446744073709551615/1/config', json={})
     assert r.status_code == 405
 
-    r = client.get('/accounts/1/18446744073709551615/config')
+    r = client.get('/accounts/18446744073709551615/1/config')
     assert r.status_code == 200
     assert r.mimetype == 'text/plain'
     assert r.charset == 'utf-8'
     assert r.cache_control.max_age > 10000
     assert r.get_data() == b''
 
-    r = client.get('/accounts/1/18446744073709551614/reachable')
+    r = client.get('/accounts/18446744073709551614/1/reachable')
     assert r.status_code == 404
     assert r.get_data() == b''
